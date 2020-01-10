92dfb012a28553d960422020b8838b8f4c734fdc
zcw <zhaochengwei968@163.com>
2020-01-10 10:50:11 +0800

    拦截器配置

---
+++ src/main/java/com/example/testyc/util/interceptor/MvcWebConfig.java
@@ -0,0 +1,18 @@
+package com.example.testyc.util.interceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcWebConfig extends WebMvcConfigurationSupport {
+    @Autowired
+    private AuthorizationInterceptor authorizationInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authorizationInterceptor).addPathPatterns("/rabbitMQ/**");
+    }
+}
